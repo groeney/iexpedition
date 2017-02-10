c406c1a4568764a88b58bcf16211ffe9eb5ca012
James Groeneveld <jamesgroeneveld@gmail.com>
2017-02-10 20:49:32 +0100

    [FEATURE] first pass at header and footer partials

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require cable
+//= require bootstrap.min
+//= require scripts
+//= require smooth-scroll.min
+//= require masonry.min
+//= require lightbox.min

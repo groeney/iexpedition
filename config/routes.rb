acf6ebb6c84118df9401f00ca21d20417915c7fb
James Groeneveld <jamesgroeneveld@gmail.com>
2017-02-21 12:23:02 +0100

    Putting up example to show foundry team

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "chewbacca" => "pages#chewbacca"
   get "search" => "pages#search"
   get "ship_listing" => "pages#ship_listing"
+  get "carousel" => "pages#carousel"
   root "pages#home"
   resources :voyages, only: [:show]
   resources :destinations, only: [:show]

227dbde3ff3807eeeb6e1bf495e5c661c4f212e8
James Groeneveld <jamesgroeneveld@gmail.com>
2017-02-23 22:44:36 +0100

    [FEATURE] added support for multi itin days

--- spec/factories/activities.rb
+++ spec/factories/activities.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :activity do
     name { ["Kayaking", "Camping", "Snow Shoeing"].sample }
     overview { Faker::Lorem.paragraph }
+    price { rand(250) }
   end
 end
--- spec/factories/itinerary_days.rb
+++ spec/factories/itinerary_days.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :itinerary_day do
-    
+    name { Faker::Name.name }
+    overview { Faker::Lorem.paragraph }
   end
 end

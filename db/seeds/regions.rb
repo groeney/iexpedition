890bb3674d0d243ffe9ad95d88ba35c985ebda76
James Groeneveld <jamesgroeneveld@gmail.com>
2017-03-16 13:30:23 +0100

    [DATA] add highlight_names to regions tab

--- db/seeds/regions.rb
+++ db/seeds/regions.rb
@@ -3,9 +3,13 @@ Dir["./db/seeds/data/*regions-#{TYPE}.csv"].each do |fn|
   new_file(fn)
   CSV.foreach(fn, :headers => true) do |row|
     region_obj = clean_data row.to_hash.symbolize_keys!
+    highlights = extract_named_resources(region_obj, :highlight_names, "Highlight")
     destination = extract_resource("Destination", region_obj, "destination_name")
     singleton = create_singleton("Region", region_obj)
 
     associate_singleton_with_collection(destination.regions, singleton) if destination.try(:valid?)
+    highlights.each do |highlight|
+      associate_singleton_with_collection(singleton.highlights, highlight) unless highlight.nil?
+    end
   end
 end

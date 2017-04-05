05e53017110927fbd4f2b99bfa9a7d853c0fcc55
kohut <kohut.den@gmail.com>
2017-04-05 12:42:35 +0300

    [FEATURE] search filters

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,7 +1,6 @@
 $(document).on('turbolinks:load', function(){
   if ($('.search.voyages, .search.ships, .search.antarctica, .search.arctic').length > 0) {
     if ($('#durationSlider').length == 0){
-      debugger
       $('#duration-slider').slider({
         formatter: function(value) {
           return 'Current value: ' + value;

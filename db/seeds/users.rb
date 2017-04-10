35f94a10ee501185eee590838eeffd17e2f21293
James Groeneveld <jamesgroeneveld@gmail.com>
2017-04-10 16:42:25 +0200

    [PATCH] user creation failing on heroku app

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,3 +1,3 @@
 test_user = User.find_or_create_by(email: "test@example.com")
-test_user.password = "password" if test_user.encrypted_password.blank?
+test_user.password = "password"
 test_user.save!

3d8279fdd3143ed52806b50acb5e72064986c27c
Sergey Volkov <volkovbro1997@gmail.com>
2017-04-13 11:59:00 +0300

    [BUG] Flash messages

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -5,3 +5,16 @@ function success_message(message) {
 function error_message(message) {
   $("#error_message").text(message).fadeIn().delay(5000).fadeOut();
 }
+
+$(document).on('turbolinks:load', function(){
+  var flashMessage = $("#flash_message");
+  if ( flashMessage.length ) {
+    var message = flashMessage.data("message");
+    var level = flashMessage.data("level");
+    if ( ['notice', 'success'].includes(level) ) {
+      success_message(message)
+    } else {
+      error_message(message)
+    }
+  }
+});
